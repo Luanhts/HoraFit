232a5fc6fcac7ca4da93f77e313396066c25437a
Luanhts <luanh3nrique08@gmail.com>
2026-01-01 18:28:33 -0300

    feat: layout base admin

---
+++ frontend/src/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import AdminLayout from "@/pages/admin/AdminLayout";
+
+export default function AdminIndexPage() {
+  return (
+    <AdminLayout>
+      <section>
+        <h1 className="text-2xl font-bold">Painel Admin</h1>
+        <p>Área administrativa — desenvolvendo funcionalidades aqui.</p>
+      </section>
+    </AdminLayout>
+  )
+  }
